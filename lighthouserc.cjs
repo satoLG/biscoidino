5643eea3c91b458b483689ada3b9304fddc74869
satoLG <leonardogsato@gmail.com>
2025-12-04 09:59:37 -0300

    fix - lighthouserc.cjs syntax for tests

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,21 +1,24 @@
-# Lighthouse CI Configuration
-url=http://127.0.0.1:4173
-
-[lighthouserc:ci]
-collect:
-  startServerCommand=npm run preview
-  startServerReadyPattern=Local:
-  url=http://127.0.0.1:4173
-  numberOfRuns=3
-
-upload:
-  target=temporary-public-storage
-
-assert:
-  preset=lighthouse:no-pwa
-  assertions:
-    categories:performance=[0.9,null]
-    categories:accessibility=[0.95,null]
-    categories:best-practices=[0.9,null]
-    categories:seo=[0.9,null]
-    categories:pwa=[0.9,null]
+// Lighthouse CI Configuration
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run preview',
+      startServerReadyPattern: 'Local:',
+      url: 'http://localhost:4173',
+      numberOfRuns: 3
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': [0.9, null],
+        'categories:accessibility': [0.95, null],
+        'categories:best-practices': [0.9, null],
+        'categories:seo': [0.9, null],
+        'categories:pwa': [0.9, null]
+      }
+    }
+  }
+};
